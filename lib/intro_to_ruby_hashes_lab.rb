f5aff86d8720d9ab0d97018086180b51c9a6014a
Elliott Stein <estein1988@gmail.com>
2020-07-06 20:27:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,8 +3,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  hash_to_read = {}
-  read_from_hash(hash_to_read, :key)
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
